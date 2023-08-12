923fb2a9963bdf18f5704aeac61e0a17b3a7ccfd
abhibhartiya <abhinayabhartiya@gmail.com>
2023-08-12 18:53:51 +0530

    Final Changes

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', event => {
-    console.log('This is service worker');
-});
-  
-self.addEventListener('fetch', event => {
-    console.log('Inside fetch', event.request.url);
-});
